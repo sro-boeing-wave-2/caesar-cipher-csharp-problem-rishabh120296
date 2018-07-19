7d243eb8c33d6ee7c4c12d5010194008a60ba628
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-19 15:34:45 +0530

    Written logic for caeser cipher conversion

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,31 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string outputString = "";
+            foreach (char letter in text)
+            {
+                int unicode = letter;
+                if (unicode >= 97 && unicode <= 122)
+                {
+                    unicode = unicode + shiftKey;
+                    if (unicode > 122)
+                    {
+                        unicode = unicode - 26;
+                    }
+                }
+                if (unicode >= 65 && unicode <= 90)
+                {
+                    unicode = unicode + shiftKey;
+                    if (unicode > 90)
+                    {
+                        unicode = unicode - 26;
+                    }
+                }
+
+
+                outputString += (char)unicode;
+            }
+            return outputString;
         }
     }
 }
